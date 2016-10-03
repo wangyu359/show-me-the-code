34845fb63f4cce4bafccbb2818bdb95fffcee012
王宇 <wangyu359@outlook.com>
2016-10-03 17:49:15 +0800

    Create 0001_others_unicity.py

---
+++ 0001_others_unicity.py
@@ -0,0 +1,52 @@
+
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+
+
+import random
+
+import string
+
+
+
+def activation_code(id,length=10):
+
+  '''
+
+  id + L + 随机码
+
+  string模块中的3个函数：string.letters，string.printable，string.printable
+
+  '''
+
+  prefix = hex(int(id))[2:]+ 'L'
+
+  length = length - len(prefix)
+
+  chars=string.ascii_letters+string.digits
+
+  return prefix + ''.join([random.choice(chars) for i in range(length)])
+
+
+
+def get_id(code):
+
+  ''' Hex to Dec '''
+
+  return str(int(code.upper(), 16))
+
+
+
+if __name__=="__main__":
+
+  for i in range(0,199,1):
+
+    code = activation_code(i)
+
+    id_hex = code.split('L')[0]
+
+    id  = get_id(id_hex)
+
+    print(code,id)
